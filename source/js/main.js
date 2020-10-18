2077184e8f057189aaf0ac3f8bf0c8be509b1533
maria <marivasi11@yandex.ru>
2020-10-18 20:30:41 +0700

    Добавляет аккордеон, убирает scroll за popup, область клика у ссылок

--- source/js/main.js
+++ source/js/main.js
@@ -3,14 +3,17 @@
 var popup = document.querySelector('.popup');
 var btnCall = document.querySelector('.navigation__contacts-call');
 var btnCloseForm = document.querySelector('.popup__btn-close');
+var body = document.querySelector('body');
 
 btnCall.addEventListener('click', function () {
   if (popup.classList.contains('popup--closed')) {
     popup.classList.remove('popup--closed');
     popup.classList.add('popup--opened');
+    body.style.overflow = 'hidden';
   } else {
     popup.classList.add('popup--closed');
     popup.classList.remove('popup--opened');
+    body.style.overflow = 'scroll';
   }
   username.focus();
 });
@@ -18,6 +21,7 @@ btnCall.addEventListener('click', function () {
 btnCloseForm.addEventListener('click', function () {
   popup.classList.add('popup--closed');
   popup.classList.remove('popup--opened');
+  body.style.overflow = 'scroll';
 });
 
 document.addEventListener('keydown', function (event) {
@@ -26,6 +30,7 @@ document.addEventListener('keydown', function (event) {
       popup.classList.add('popup--closed');
       popup.classList.remove('popup--opened');
       popup.classList.remove('popup--error');
+      body.style.overflow = 'scroll';
     }
   }
 });
@@ -40,41 +45,7 @@ popup.addEventListener('click', function (event) {
   }
 });
 
-var btnSection = document.querySelector('.page-footer-btn__sec');
-var sectionList = document.querySelector('.page-footer__nav-list');
-
-btnSection.addEventListener('click', function () {
-  if (sectionList.classList.contains('page-footer__closed')) {
-    sectionList.classList.remove('page-footer__closed');
-    sectionList.classList.add('page-footer__opened');
-    btnSection.classList.remove('page-footer-btn__sec');
-    btnSection.classList.add('page-footer-btn__sec--opened');
-  } else {
-    sectionList.classList.add('page-footer__closed');
-    sectionList.classList.remove('page-footer__opened');
-    btnSection.classList.remove('page-footer-btn__sec--opened');
-    btnSection.classList.add('page-footer-btn__sec');
-  }
-});
-
-var btnContact = document.querySelector('.page-footer-btn__acc');
-var contactList = document.querySelector('.page-footer-contacts__list');
-
-btnContact.addEventListener('click', function () {
-  if (contactList.classList.contains('page-footer__closed')) {
-    contactList.classList.remove('page-footer__closed');
-    contactList.classList.add('page-footer__opened');
-    btnContact.classList.remove('page-footer-btn__acc');
-    btnContact.classList.add('page-footer-btn__acc--opened');
-  } else {
-    contactList.classList.add('page-footer__closed');
-    contactList.classList.remove('page-footer__opened');
-    btnContact.classList.remove('page-footer-btn__acc--opened');
-    btnContact.classList.add('page-footer-btn__acc');
-  }
-});
-
-
+// скролл
 var linkNav = document.querySelectorAll('[href^="#"]');
 var V = 0.5;
 for (var i = 0; i < linkNav.length; i++) {
@@ -101,6 +72,7 @@ for (var i = 0; i < linkNav.length; i++) {
   }, false);
 }
 
+// localStorage
 var username = popup.querySelector('[name=username]');
 var phone = popup.querySelector('[name=phone]');
 var popupField = popup.querySelector('[name=popup-field]');
@@ -121,3 +93,38 @@ questionaryForm.addEventListener('submit', function () {
 });
 
 $('input[type=tel]').mask('+ 7 (999) 999-9999');
+
+// аккордеон
+var list = document.querySelector('.page-footer__nav-list');
+var btnList = document.querySelector('.page-footer-btn__sec');
+var listContact = document.querySelector('.page-footer-contacts__list');
+var btnListContact = document.querySelector('.page-footer-btn__acc');
+
+list.classList.remove('page-footer__nojs');
+listContact.classList.remove('page-footer__nojs');
+btnList.style.display = 'none';
+btnListContact.style.display = 'none';
+
+var init = function () {
+  var element = document.getElementsByClassName('page-footer__accordeon__heading');
+  for (var k = 0, elemLength = element.length; k < elemLength; k++) {
+    element[k].addEventListener('click', toggleSlide, false);
+  }
+};
+
+var toggleSlide = function (e) {
+  removeClass();
+  var elem = e.target;
+  elem.classList.add('page-footer__accordeon__heading_active');
+};
+
+var removeClass = function () {
+  var elementRemovedClass = document.getElementsByClassName('page-footer__accordeon__heading');
+  var elementLength = elementRemovedClass.length;
+
+  for (var j = 0; j < elementLength; j++) {
+    elementRemovedClass[j].classList.remove('page-footer__accordeon__heading_active');
+  }
+};
+
+init();
